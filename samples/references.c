41958e007e067afeb9d4c254bd5789a4be871d4a
Joe Hu <vegertar@gmail.com>
2024-03-19 18:30:41 +0800

    Test ref in macro expansion.

--- samples/references.c
+++ samples/references.c
@@ -1,4 +1,36 @@
 void foo(int
 n) {
 n++;
+int
+x,
+y;
+
+struct bar {
+  int
+n;
+};
+
+struct foo {
+  struct bar
+bar[4];
+};
+
+struct foo
+foo;
+
+(void)
+foo.
+bar[
+n].
+n;
+
+#define N \
+ foo.\
+ bar[\
+ n-1 + \
+ y + \
+ x + 1].\
+ n
+(void)
+N;
 }
--- spec/references_spec.sh
+++ spec/references_spec.sh
@@ -8,12 +8,16 @@ Context
   AfterAll 'cleanup'
 
   query_decl_number() {
-    echo SELECT decl FROM tok WHERE begin_row = $1 AND begin_col = $2
+    echo SELECT decl FROM tok WHERE begin_row = $1 AND begin_col = $2 AND offset = ${3:-0}
   }
 
   Describe 'Consistent decl'
     Parameters
       n 3 1 2 1 # reference parameters
+      n 24 1 2 1 # array subscripts
+      n 25 1 10 1 # struct fields
+      foo 22 1 19 1 # local variables
+      bar 23 1 15 1 # struct fields
     End
 
     query() {
@@ -28,6 +32,33 @@ Context
 
     It "compares the decl $1"
       When call query "$2" "$3" "$4" "$5"
+      The output should eq 0
+    End
+  End
+
+  Describe 'Consistent decl in macro expansion'
+    Parameters
+      N 35 1 0 # the macro expansion point
+      foo 28 2 1 19 1 # the 1st token after the expansion point
+      bar 29 2 3 15 1 # the 3ed
+      n 30 2 5 2 1    # the 5th
+      y 31 2 9 6 1    # the 9th
+      x 32 2 11 5 1   # the 11th
+      n 33 2 16 10 1  # the 16th
+    End
+
+    query() {
+      a=$(sqlite3 $db "`query_decl_number $1 $2 $3`")
+      if [ $3 -eq 0 ]; then
+        b=-1
+      else
+        b=$(sqlite3 $db "`query_decl_number $4 $5`")
+      fi
+      echo $((a-b))
+    }
+
+    It "compares the decl $1"
+      When call query "$2" "$3" "$4" "$5" "$6"
       The output should eq 0
     End
   End
